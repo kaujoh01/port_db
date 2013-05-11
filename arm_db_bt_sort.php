c175a488245de09326d2e3cf1b921f0d4b38c4dd
Kauser Johar <kauser.johar@gmail.com>
2013-05-11 18:29:00 +0100

    Completed bt list script

--- arm_db_bt_sort.php
+++ arm_db_bt_sort.php
@@ -91,10 +91,12 @@ while($db_field = mysql_fetch_array($db_result))
 $file = @fopen('arm_crick_player_list.csv', "r") or exit("Unable to open file!");
 $i=0;
 $split = array();
+$order = 0;
 while(!feof($file))
 {
   $curr_line = fgets($file);
   $split = preg_split('%[,]+%', $curr_line);
+  // Extract info for the line
   foreach ($split as $key => $val) {
     switch ($key)
       {
@@ -113,6 +115,15 @@ while(!feof($file))
       case 12: $bt_match_type[$i]   = $val; break;
       }
   }
+  // Derive the order
+  if ($i==0) {
+    $order = 1;
+  } else if ($bt_date[$i]!=$bt_date[$i-1]) {
+    $order = 1;
+  } else {
+    $order = $order +1;
+  }
+  $bt_order[$i] = $order;
   // Get batsman ID
   $bt_id[$i] = get_player_id($bt_name);
   // Get the match ID
@@ -129,6 +140,35 @@ while(!feof($file))
     // temporarily assign a 0 match id
     $bt_match_id[$i] = 0;
   }
+  // Generate if the current player is innings 1 or innings 2
+  if ($bt_match_id[$i]==0) {
+    $bt_inn_type[$i] = 3;
+  } else if ( ($match_list_opp_a_id[$bt_match_id[$i]]==1) &&
+	      ($match_list_opp_b_id[$bt_match_id[$i]]==1)) {
+    // This is an ARM vs ARM game
+    if ($bt_order[$i]>11) {
+      $bt_inn_type[$i] = 2;
+    } else {
+      $bt_inn_type[$i] = 1;
+    }
+  } else {
+    // This is a normal game so find out if ARM was innings1
+    // or innings2
+    if ($match_list_opp_a_id[$bt_match_id[$i]]==1) {
+      $bt_inn_type[$i] = 1;
+    } else if ($match_list_opp_b_id[$bt_match_id[$i]]==1) {
+      $bt_inn_type[$i] = 2;
+    } else {
+      $bt_inn_type[$i] = 4;
+    }
+  }
+  // Error checking for bt inn type
+  if ($bt_inn_type[$i]==3) {
+    echo "ERROR: Innings (code - 3) could not be derived for date $bt_date[$i]<br/>";
+  }
+  if ($bt_inn_type[$i]==4) {
+    echo "ERROR: Innings (code - 4) could not be derived for date $bt_date[$i]<br/>";
+  }
   // Update i to next
   $i=$i+1;
 }
@@ -138,8 +178,11 @@ if ($debug_msg!=0) {
   for ($i=0;$i<$player_num_lines;$i++) {
     $bt_name = $player_list_name[$bt_id[$i]];
     $how_out_name = $how_out_list_name[$bt_how_out_id[$i]];
+    $order = $bt_order[$i];
     echo "id==$i,
+          order==$order,
           match_id==$bt_match_id[$i],
+          innings==$bt_inn_type[$i],
           date==$bt_date[$i],
           name==$bt_name,
           runs==$bt_runs_scored[$i],
@@ -156,12 +199,48 @@ if ($debug_msg!=0) {
 fclose($file);
 // ------------------------------------------------------------------
 // ------------------------------------------------------------------
+// Update innings 1 or innings 2 batting list
+// ------------------------------------------------------------------
+$SQL="TRUNCATE opp_a_bt_list";
+$result_sql=mysql_query($SQL);
+$SQL="TRUNCATE opp_b_bt_list";
+$result_sql=mysql_query($SQL);
+
+for ($i=0;$i<$player_num_lines;$i++) {
+  $put_match_id = $bt_match_id[$i];
+  $put_order = $bt_order[$i];
+  $put_bt_id = $bt_id[$i];
+  $put_num_runs = $bt_runs_scored[$i];
+  $put_num_balls = 0;
+  $put_num_4s = 0;
+  $put_num_6s = 0;
+  $put_out_id = $bt_how_out_id[$i];
+  $put_bl_id = 0;
+  $put_c_ro_id = 0;
+  $bt_list_field="`match_id`, `order`, `bt_id`, `num_runs`, `num_balls`, `num_4s`, `num_6s`, `out_id`, `bl_id`, `c_ro_id`";
+  $bt_list_value="$put_match_id, $put_order, $put_bt_id, $put_num_runs, $put_num_balls, $put_num_4s, $put_num_6s, $put_out_id, $put_bl_id, $put_c_ro_id";
+  //
+  // Update the DB
+  //
+  if ($bt_inn_type[$i]==1) {
+    $SQL="INSERT INTO `$database`.`opp_a_bt_list` ($bt_list_field) VALUES ($bt_list_value)";
+    $result_sql=mysql_query($SQL);
+    //echo "$SQL<br />";
+  } else if ($bt_inn_type[$i]==2) {
+    $SQL="INSERT INTO `$database`.`opp_b_bt_list` ($bt_list_field) VALUES ($bt_list_value)";
+    $result_sql=mysql_query($SQL);
+    //echo "$SQL<br />";
+  } else {
+    echo "ERROR: Couldn't enter data into batting list <br />";
+  }
+}
+// ------------------------------------------------------------------
+// ------------------------------------------------------------------
 // Close session
 // ------------------------------------------------------------------
 //
 mysql_close($db_handle);
 //
 // ------------------------------------------------------------------
-
 // --CODE ENDS HERE--
 ?>
